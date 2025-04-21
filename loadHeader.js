5be6b96d6f66b61b16caf1fd6a7f67381d4d9e34
ppatil1708 <write.prerna@gmail.com>
2025-04-21 12:15:45 -0700

    Load the header correctly

--- loadHeader.js
+++ loadHeader.js
@@ -1,5 +1,5 @@
 // Load the header dynamically
-fetch('./header.html') // Increment the version number if needed
+fetch('/header.html') // Increment the version number if needed
     .then(response => response.text())
     .then(data => document.getElementById('header').innerHTML = data)
     .catch(error => console.error('Error loading header:', error));
